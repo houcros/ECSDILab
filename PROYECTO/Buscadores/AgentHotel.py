80ee595ac6c785af7f902cc62ce081306b268653
Ignacio N. Lucero Ascencio <igwisnirrol@gmail.com>
2015-05-17 01:10:40 +0200

    AgentHotel

--- PROYECTO/Buscadores/AgentHotel.py
+++ PROYECTO/Buscadores/AgentHotel.py
@@ -17,6 +17,7 @@ acceso a EAN (EANCID, EANKEY)
 
 __author__ = 'javier'
 
+import json
 import requests
 import urllib2
 import md5
@@ -40,21 +41,31 @@ sig = md5.new(EAN_KEY + EAN_SECRET + timestamp).hexdigest()
 
 r = requests.get(EAN_END_POINT,
                  params={'cid': EAN_DEV_CID, 'minorRev': 29, 'apiKey': EAN_KEY, 'sig': sig,
-                 		'locale': 'es_ES', 'currencyCode': 'EUR', 'numberOfResults': 10,            'latitude': '041.40000', 'longitude': '002.16000',
-                         'searchRadius': 2, 'searchRadiusUnit': 'KM',
-                         'arrivalDate': '06/02/2015', 'departureDate': '06/08/2015'
-                       })
+                 		'locale': 'es_ES', 'currencyCode': 'EUR', 'numberOfResults': 2,
+                 		'latitude': '041.40000', 'longitude': '002.16000',
+                        'searchRadius': 2, 'searchRadiusUnit': 'KM',
+                        'arrivalDate': '06/02/2015', 'departureDate': '06/08/2015'
+                    	})
 
 #print r.text
 dic = r.json()
+#print json.dumps(dic, indent=4, sort_keys=True)
 
 if 'EanWsError' in dic['HotelListResponse']:
-	print 'Error de tipo ' + dic['HotelListResponse']['EanWsError']['category'] + ' => ' + dic['HotelListResponse']['EanWsError']['verboseMessage']
+	print ('Error de tipo ' + dic['HotelListResponse']['EanWsError']['category'],
+		' => ' + dic['HotelListResponse']['EanWsError']['verboseMessage'])
 else:
 	for hot in dic['HotelListResponse']['HotelList']['HotelSummary']:
-		print hot['name']
-
-
+		print ("Hotel " + hot['name'],
+			", distancia del centro: " + '{:.2f}'.format(hot['proximityDistance']),
+			' ' + hot['proximityUnit'] + ', precio total: ',
+			hot['RoomRateDetailsList']['RoomRateDetails']['RateInfos']['RateInfo']['ChargeableRateInfo']['@total'],
+			', rating: ' + '{:.1f}'.format(hot['hotelRating']),
+			', tripAdvisorRating: ' + '{:.1f}'.format(hot['tripAdvisorRating']),
+			' tripAdvisorReviewCount: ' + '{:.0f}'.format(hot['tripAdvisorReviewCount'])
+			)
+		print
+		
 
 
 
